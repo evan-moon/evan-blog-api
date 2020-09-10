307bbf323994362b75b8abd4aecb8df8450a0b7c
evan-moon <bboydart91@gmail.com>
2020-09-11 00:09:00 +0900

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,83 @@
+module.exports = {
+  root: true,
+  globals: {
+    __PATH_PREFIX__: true,
+  },
+  env: {
+    es6: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'eslint-config-prettier', 'react-app'],
+  plugins: ['@typescript-eslint', 'import', 'prettier'],
+  settings: { 'import/resolver': { typescript: {} } },
+  rules: {
+    'prettier/prettier': 'error',
+    'no-implicit-coercion': 'error',
+    'no-undef': 'off',
+    indent: 'off',
+    '@typescript-eslint/indent': 'off',
+    semi: 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-extra-boolean-cast': 'off',
+    'getter-return': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-parameter-properties': 'off',
+    'no-restricted-imports': [
+      'error',
+      {
+        paths: [
+          {
+            name: 'util',
+            importNames: ['isArray'],
+            message: '`Array.isArray`를 대신 사용해주세요!',
+          },
+        ],
+      },
+    ],
+    'no-async-promise-executor': 'warn',
+    '@typescript-eslint/prefer-as-const': 'warn',
+    '@typescript-eslint/no-non-null-asserted-optional-chain': 'warn',
+    '@typescript-eslint/ban-types': 'warn',
+    '@typescript-eslint/no-inferrable-types': 'warn',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      { format: ['camelCase', 'UPPER_CASE', 'PascalCase'], selector: 'variable', leadingUnderscore: 'allow' },
+      { format: ['camelCase', 'PascalCase'], selector: 'function' },
+      { format: ['PascalCase'], selector: 'interface' },
+      { format: ['PascalCase'], selector: 'typeAlias' },
+    ],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/array-type': ['error', { default: 'array-simple' }],
+    '@typescript-eslint/no-unused-vars': ['error', { ignoreRestSiblings: true }],
+    '@typescript-eslint/member-ordering': [
+      'error',
+      {
+        default: [
+          'public-static-field',
+          'private-static-field',
+          'public-instance-field',
+          'private-instance-field',
+          'public-constructor',
+          'private-constructor',
+          'public-instance-method',
+          'private-instance-method',
+        ],
+      },
+    ],
+    'no-warning-comments': [
+      'warn',
+      {
+        terms: ['TODO', 'FIXME', 'XXX', 'BUG'],
+        location: 'anywhere',
+      },
+    ],
+    'prefer-const': 'error',
+    'no-var': 'error',
+    curly: ['error', 'all'],
+    eqeqeq: ['error', 'always', { null: 'ignore' }],
+  },
+};
